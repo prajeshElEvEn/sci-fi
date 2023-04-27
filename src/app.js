689a06cdad1c29400bbfadaf3b96ca6c2e938426
prajeshElEvEn <prajesh.eleven118@gmail.com>
2023-04-27 22:28:24 +0530

    - added script

--- src/app.js
+++ src/app.js
@@ -4,13 +4,13 @@ const context = canvas.getContext("2d")
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 
-window.addEventListener("resize", () => {
+window.addEventListener("resize", function () {
     canvas.width = window.innerWidth
     canvas.height = window.innerHeight
     render();
 })
 
-const files = (index) => {
+function files(index) {
     var data = `
     ../assets/images/male0001.png
     ../assets/images/male0002.png
@@ -318,9 +318,65 @@ const files = (index) => {
 
 const frameCount = 300
 const images = []
-const imageSeq = {
+const imageSequence = {
     frame: 1
 }
+
+for (let i = 0; i < frameCount; i++) {
+    const img = new Image()
+    img.src = files(i)
+    images.push(img)
+}
+
+gsap.to(imageSequence, {
+    frame: frameCount - 1,
+    snap: "frame",
+    ease: `none`,
+    scrollTrigger: {
+        scrub: 0.15,
+        trigger: `#page>canvas`,
+        start: `top top`,
+        end: `600% top`,
+        scroller: `#main`,
+    },
+    onUpdate: render
+})
+
+images[1].onload = render
+
+function render() {
+    scaleImage(images[imageSequence.frame], context)
+}
+
+function scaleImage(img, ctx) {
+    var canvas = ctx.canvas
+    var hRatio = canvas.width / img.width
+    var vRatio = canvas.height / img.height
+    var ratio = Math.max(hRatio, vRatio)
+    var centerShift_x = (canvas.width - img.width * ratio) / 2
+    var centerShift_y = (canvas.height - img.height * ratio) / 2
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    ctx.drawImage(
+        img,
+        0,
+        0,
+        img.width,
+        img.height,
+        centerShift_x,
+        centerShift_y,
+        img.width * ratio,
+        img.height * ratio
+    )
+}
+
+ScrollTrigger.create({
+    trigger: `#page>canvas`,
+    pin: true,
+    markers: true,
+    scroller: `#main`,
+    start: `top top`,
+    end: `600% top`,
+})
 
 gsap.to("#page1", {
     scrollTrigger: {
